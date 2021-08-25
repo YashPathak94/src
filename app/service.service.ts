616c2ff1c202f2a78e23819c10e39de056c70cac
unknown <abhinav.purwar173@gmail.com>
2021-08-25 21:35:53 +0530

    changes

--- app/service.service.ts
+++ app/service.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getFirwareDetails(): Observable<any>{
+    return this.http.get<any>('http://localhost:8080/');
+
+  }
 }
--- app/user/user.component.ts
+++ app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from 'app/service.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appService:ServiceService) { }
 
   ngOnInit(): void {
   }
-
+  loadDoc(){
+    this.appService.getFirwareDetails().subscribe(data => {
+      console.log(data);
+    });
+  }
 }
